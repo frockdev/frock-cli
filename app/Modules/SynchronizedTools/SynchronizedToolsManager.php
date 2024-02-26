ab951b4f7bc140a72d9be3fc175c58ef8de7cba6
Vladimir <vladimir@sobaka.com>
2024-02-26 22:25:42 +0100

    bumpers and only

--- app/Modules/SynchronizedTools/SynchronizedToolsManager.php
+++ app/Modules/SynchronizedTools/SynchronizedToolsManager.php
@@ -33,9 +33,14 @@ class SynchronizedToolsManager
         $repo = $git->cloneRepository($tool->link, $toolDir);
 
         $repo->checkout($tool->version);
-        foreach ($tool->excludePaths as $excludePath) {
-            shell_exec('rm -rf '.$toolDir.'/'.$excludePath);
+        if (count($tool->excludePaths)>0 && count($tool->onlyPaths)===0) {
+            foreach ($tool->excludePaths as $excludePath) {
+                shell_exec('rm -rf '.$toolDir.'/'.$excludePath);
+            }
+        } elseif (count($tool->onlyPaths)>0) {
+            $this->removeAllExcept($toolDir, $tool->onlyPaths);
         }
+
 
         foreach ($tool->copyPaths as $copyPath) {
             shell_exec('cp -r '.$toolDir.'/'.$copyPath->from.' '.$toolDir.'/'.$copyPath->to);
@@ -49,6 +54,30 @@ class SynchronizedToolsManager
         foreach ($tool->gitignore as $gitignore) {
             file_put_contents($toolDir.'/.gitignore', $gitignore.PHP_EOL , FILE_APPEND | LOCK_EX);
         }
+    }
+
+    public function findHighestToolMajorVersion(SynchronizedTool $tool) {
+        $config = app()->make(Config::class);
+        $git = new \CzProject\GitPhp\Git;
+        $tmpToolDir = $config->getWorkingDir().'/'. $tool->name.'-tmp-synchronized-tool';
+        shell_exec('rm -rf '.$tmpToolDir);
+        $repo = $git->cloneRepository($tool->link, $tmpToolDir);
+        $tags = $repo->getTags();
+        shell_exec('rm -rf '.$tmpToolDir);
+        $currentVersion = $tool->version;
+        return $this->findHighestMajorTag($tags, $currentVersion);
+    }
+
+    public function findHighestToolMinorVersion(SynchronizedTool $tool) {
+        $config = app()->make(Config::class);
+        $git = new \CzProject\GitPhp\Git;
+        $tmpToolDir = $config->getWorkingDir().'/'. $tool->name.'-tmp-synchronized-tool';
+        shell_exec('rm -rf '.$tmpToolDir);
+        $repo = $git->cloneRepository($tool->link, $tmpToolDir);
+        $tags = $repo->getTags();
+        shell_exec('rm -rf '.$tmpToolDir);
+        $currentVersion = $tool->version;
+        return $this->findHighestMinorTag($tags, $currentVersion);
     }
 
     public function findHighestToolPatchVersion(SynchronizedTool $tool)
@@ -97,5 +126,87 @@ class SynchronizedToolsManager
             return 'v'.$highestPatchedVersion;
         }
         return $highestPatchedVersion;
+    }
+
+    /**
+     * @param string $baseDir
+     * @param array $onlyPaths
+     * @return void
+     * This function should recursively scan directory and remove all files except those in $onlyPaths
+     */
+    private function removeAllExcept(string $baseDir, array $onlyPaths)
+    {
+        $files = scandir($baseDir);
+        foreach ($files as $file) {
+            if ($file === '.' || $file === '..') {
+                continue;
+            }
+            if (is_dir($baseDir.'/'.$file)) {
+                $this->removeAllExcept($baseDir.'/'.$file, $onlyPaths);
+            } else {
+                if (!in_array($file, $onlyPaths)) {
+                    shell_exec('rm -rf '.$baseDir.'/'.$file);
+                }
+            }
+        }
+    }
+
+    private function findHighestMajorTag(array $tags, string $currentVersion) {
+        $vIsFirst = false;
+        if (str_starts_with($currentVersion, 'v')) {
+            $vIsFirst = true;
+            $currentVersion = substr($currentVersion, 1);
+        }
+        $currentVersion = explode('.', $currentVersion);
+        $currentVersion = array_map('intval', $currentVersion);
+        $highestMajor = 0;
+        foreach ($tags as $tag) {
+            if ($vIsFirst) {
+                $tag = substr($tag, 1);
+            }
+            $tag = explode('.', $tag);
+            $tag = array_map('intval', $tag);
+            if ($tag[0] >= $currentVersion[0]) {
+                if ($tag[0] >= $highestMajor) {
+                    $highestMajor = $tag[0];
+                }
+            }
+        }
+        unset($currentVersion[0]);
+        $highestMajoredVersion = $highestMajor.'.0.0';
+        if ($vIsFirst) {
+            return 'v'.$highestMajoredVersion;
+        }
+        return $this->findHighestMinorTag($tags, $highestMajoredVersion);
+    }
+
+    private function findHighestMinorTag(?array $tags, string $currentVersion)
+    {
+        $vIsFirst = false;
+        if (str_starts_with($currentVersion, 'v')) {
+            $vIsFirst = true;
+            $currentVersion = substr($currentVersion, 1);
+        }
+        $currentVersion = explode('.', $currentVersion);
+        $currentVersion = array_map('intval', $currentVersion);
+        $highestMinor = 0;
+        foreach ($tags as $tag) {
+            if ($vIsFirst) {
+                $tag = substr($tag, 1);
+            }
+            $tag = explode('.', $tag);
+            $tag = array_map('intval', $tag);
+            if ($tag[0] === $currentVersion[0] && $tag[1] >= $currentVersion[1]) {
+                if ($tag[1] >= $highestMinor) {
+                    $highestMinor = $tag[1];
+                }
+            }
+        }
+        unset($currentVersion[1]);
+        $highestMinoredVersion = $currentVersion[0].'.'.$highestMinor.'.0';
+        if ($vIsFirst) {
+            return 'v'.$highestMinoredVersion;
+        }
+        return $this->findHighestPatchTag($tags, $highestMinoredVersion);
     }
 }
